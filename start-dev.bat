84f3d8467f6eae5e2bed483322f38f7e472cc625
anooop05 <anoopchitneni@gmail.com>
2026-09-17 10:42:41 +0530

    removed unwanted file

--- start-dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo ==============================================
-echo   Starting Nexus Backend and Frontend together
-echo ==============================================
-npx -y concurrently -n "BACKEND,FRONTEND" -c "cyan,magenta" "npm run dev --prefix nexus-backend" "npm run dev --prefix nexus-frontend"
-pause
